2bbb01e0968a9af7b587cb0c55db24b389b9bfaf
1234Sumuu <41938215+1234Sumuu@users.noreply.github.com>
2023-05-22 12:35:30 +0600

    sumOfAllarray.java

--- sumOfAllarray.java
+++ sumOfAllarray.java
@@ -1,1 +1,11 @@
-
+public class sumOfArray{
+  public static void main(String[] args){
+    int[] numbers = {4, 3, 6, 9, 2};
+    int sum = 0;
+    
+    for(int i =0; i < numbers.length; i++){
+      sum += numbers[i];
+    }
+    System.out.println("Total Sum: "+ sum);
+  }
+}
