f7209b2819a442696af57b1714a9d3f5ac9b574c
1234Sumuu <41938215+1234Sumuu@users.noreply.github.com>
2023-05-22 12:29:20 +0600

    maxNumArray.java

--- maxNumArray.java
+++ maxNumArray.java
@@ -1,9 +1,9 @@
 public class maxElementFinder{
   public static int findMaxNumber(string[] arg){
-    int[] arr = { 4, 3, 7, 0, 9, 4};
+    int[] nums = { 4, 3, 7, 0, 9, 4};
     int max = nums[0];
     
-    for(int i = o; i < nums.length; i++){
+    for(int i = 0; i < nums.length; i++){
       if(nums[i] > max){
         max = nums[i];
       }
