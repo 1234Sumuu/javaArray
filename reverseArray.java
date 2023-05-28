ceb2963de4f121b8d7666242eefbc7b81af5597c
1234Sumuu <41938215+1234Sumuu@users.noreply.github.com>
2023-05-28 10:55:10 +0600

    reverseArray.java

--- reverseArray.java
+++ reverseArray.java
@@ -1,1 +1,25 @@
+import java.util.Arrays;
 
+public class ArrayReverse{
+  public static void main(String[] args){
+    int[] arr = {1, 2, 3, 4, 5};
+    
+      System.out.println("Array list are: " + Arrays.toString(arr));
+      reverseArray(arr);
+    
+      System.out.println("Reverse array list are: " + reverseArray(arr));
+  }
+  public static void reverseArray(int[] arr){
+    int Start = 0;
+    int end = arr.length-1;
+    
+    while(start < end){
+      int temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      
+      start++;
+      end--;
+    }
+  }
+}
