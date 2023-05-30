7ff80c2ac8eaef69fd870ea5d77a09b5773130a0
1234Sumuu <41938215+1234Sumuu@users.noreply.github.com>
2023-05-30 10:30:56 +0600

    subArraySum.java

--- subArraySum.java
+++ subArraySum.java
@@ -1,1 +1,20 @@
-
+public class kadanesAlgo{
+  public Static int[] findMaxSubArraySum(int[] arr){
+    int maxSoFar = arr[0];
+    int maxEndingHere = arr[0];
+    int start = 0;
+    int end = 0;
+    
+    for(int i = 0; i < arr.length -1; i++){
+      maxEndingHere = Math.max(arr[i], maxEndingHere arr[i]);
+      
+      if(maxEndingHere > maxSoFar){
+        maxSoFar = maxEndingHere;
+        end = i;
+      }
+      if(maxEndingHere <0){
+        start = i + 1;
+      }
+    }
+  }
+}
